b6f82795c91bb78c0e414ca70de1ce3c72889d5c
Omar Kamal <e.omarkamal@gmail.com>
2020-09-14 00:15:32 +0300

    Signup page

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express  = require("express");
-const bodyParser = requrie("body-parser");
+const bodyParser =require("body-parser");
 const https = require("https");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+   const firstName = req.body.firstName;
+   const lastName = req.body.lastName;
+   const email = req.body.email;
+   console.log(firstName, lastName, email);
+});
 
 app.listen(3000, function(){
    console.log("Server is running on port 3000");

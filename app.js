a188afd4f27f2b4077a4b4b47821c65ccc2a1ba7
Omar Kamal <e.omarkamal@gmail.com>
2020-09-17 00:57:27 +0300

    Newsletter final commit

--- app.js
+++ app.js
@@ -16,9 +16,48 @@ app.post("/", function(req, res){
    const firstName = req.body.firstName;
    const lastName = req.body.lastName;
    const email = req.body.email;
-   console.log(firstName, lastName, email);
+
+   var data = {
+      members: [
+         {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+               FNAME: firstName,
+               LNAME: lastName
+            }
+         }
+      ],
+   };
+
+   var jsonData = JSON.stringify(data);
+
+   const url =  "https://us2.api.mailchimp.com/3.0/lists/ac0fc066d0";
+
+   const options = {
+     method: "POST",
+     auth: "omar1:e926b71d65f4a17895db29f8e4b813e1-us2"
+   };
+
+   const request = https.request(url, options, function(response){
+
+      if(response.statusCode === 200){
+         res.sendFile(__dirname + "/success.html");
+      }else {
+         res.sendFile(__dirname + "/failure.html");
+      }
+
+      response.on("data", function(data){
+         console.log(JSON.parse(data));
+      });
+   });
+
+   request.write(jsonData);
+   request.end();
+
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
    console.log("Server is running on port 3000");
 });
+
